165ba4a61ec45acb056de4fd44031a2a4f00643f
Hyemin Heo <heohm128@gmail.com>
2023-03-02 02:18:57 +0900

    [Style] 회원정보 마이페이지에 나타나도록 연결

--- CheckPlay/CheckPlay/View/MyPage/MyPageView.swift
+++ CheckPlay/CheckPlay/View/MyPage/MyPageView.swift
@@ -6,17 +6,33 @@
 //
 
 import SwiftUI
+import AlertToast
 
 struct MyPageView: View {
+    @EnvironmentObject var userStore: UserStore
+    @State var isPresentedLogOutFailureToastAlert: Bool = false
     var body: some View {
         VStack {
+            Text("\(userStore.currentUser?.name ?? "N/A")")
+            Text("\(userStore.currentUser?.studentCode ?? "N/A")")
             
+            Button(action: {}) {
+                Text("회원정보 수정")
+            }
+            
+            Button(action: {
+                let logOutResult = userStore.logOut()
+                if !logOutResult { isPresentedLogOutFailureToastAlert = true }
+            }) {
+                Text("로그아웃")
+            }
+            .buttonStyle(.borderedProminent)
+            
+           
+        }
+        .toast(isPresenting: $isPresentedLogOutFailureToastAlert) {
+            AlertToast(displayMode: .alert, type: .error(.red), title: "로그아웃 실패", subTitle: "다시 시도해주세요.")
         }
     }
 }
 
-struct MyPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        MyPageView()
-    }
-}
