1b9705c6e8de2e369b1bb29b5e0a1344af6ed780
Hyemin Heo <heohm128@gmail.com>
2023-03-02 02:20:18 +0900

    [Add] 파이어베이스 필드 상수로 선언

---
+++ CheckPlay/CheckPlay/Constant/UserConstant.swift
@@ -0,0 +1,16 @@
+//
+//  UserConstant.swift
+//  CheckPlay
+//
+//  Created by sole on 2023/02/28.
+//
+
+import Foundation
+
+/// user 모델을 파이어베이스에서 읽고 쓸 때 사용하는 필드 이름들 입니다.
+struct UserConstant {
+    static let userID: String = "id"
+    static let userStudentCode: String = "student_code"
+    static let userEmail: String = "email"
+    static let userName: String = "name"
+}
