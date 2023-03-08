9b7863402501d10cbf57ee92d9c70ee5bb501b53
Hyemin Heo <heohm128@gmail.com>
2023-03-09 00:18:59 +0900

    [Style/Feat] 전체 뷰 연결을 위한 설정 코드 추가

--- CheckPlay/CheckPlay/CheckPlayApp.swift
+++ CheckPlay/CheckPlay/CheckPlayApp.swift
@@ -7,15 +7,19 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
+import NMapsMap
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        NMFAuthManager.shared().clientId = "\(Bundle.main.object(forInfoDictionaryKey: "NAVER_API_CLIENT_ID") ?? "")"
         
         return true
     }
+    
+   
 }
 
 @main
@@ -23,19 +27,24 @@ struct CheckPlayApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var userStore: UserStore = .init()
-    
+    @StateObject var scheduleStore: ScheduleStore = .init()
+    @StateObject var attendanceStore: AttendanceStore = .init()
+    @StateObject var requestStore: RequestStore = .init()
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            
                 LoginRouteView()
                     .environmentObject(userStore)
+                    .environmentObject(scheduleStore)
+                    .environmentObject(attendanceStore)
+                    .environmentObject(requestStore)
                     .task {
                         // 이미 로그인한 상태면
                         guard let currentUser = Auth.auth().currentUser else { return }
                         await userStore.fetchUser(uid: currentUser.uid)
                         userStore.isLogin = true
                     }
-            }
+            
         }
     }
 }
