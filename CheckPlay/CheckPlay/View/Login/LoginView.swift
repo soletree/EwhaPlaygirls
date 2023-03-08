7b06729bb8c98073ee26fce53e9af02c4a11ab61
Hyemin Heo <heohm128@gmail.com>
2023-03-09 00:17:24 +0900

    [Style] 로그인 뷰 커스텀 버튼, 텍스트 필드 적용

--- CheckPlay/CheckPlay/View/Login/LoginView.swift
+++ CheckPlay/CheckPlay/View/Login/LoginView.swift
@@ -18,28 +18,34 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
-            TextField("Type your email address", text: $userEmail)
-            SecureField("Type your password", text: $userPassword)
+            Image(systemName: "baseball.fill")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: UIScreen.screenHeight * 0.25, height: UIScreen.screenHeight * 0.25)
+                .padding(10)
             
-            Button(action: {
+            CustomTextField(style: .plain, title: "이메일을 입력하세요", text: $userEmail).customTextField
+            CustomTextField(style: .secure, title: "비밀번호를 입력하세요", text: $userPassword).customTextField
+                .padding(.bottom, 20)
+            
+            CustomButton(style: .login) {
                 Task {
                    let isLoginSuccess = await userStore.logIn(email: userEmail, password: userPassword)
                     if !isLoginSuccess {
                         isPresentedLoginFailureAlert = true
                     }
                 }
-            }) {
-                Text("Submit")
-            }
-            .buttonStyle(.borderedProminent)
+            }.customButton
+            
             
             NavigationLink(destination: SignUpView().environmentObject(userStore)) {
                 Text("회원가입")
+                    .foregroundColor(.customLightGreen)
             }
             
         } // - VStack
         .toast(isPresenting: $isPresentedLoginFailureAlert) {
-            AlertToast(displayMode: .alert, type: .error(.red), title: "로그인에 실패했습니다! 다시 시도해주세요.")
+            AlertToast(displayMode: .alert, type: .error(.red), title: "로그인에 실패했습니다!\n 다시 시도해주세요")
         }
     }
 }
