09231e776bed5ca4f0d97b26f9d0e740f3ddad34
Hyemin Heo <heohm128@gmail.com>
2023-03-09 00:15:08 +0900

    [Style] 커스텀 버튼 스타일 구현

--- CheckPlay/CheckPlay/View/Component/CustomButton.swift
+++ CheckPlay/CheckPlay/View/Component/CustomButton.swift
@@ -7,25 +7,81 @@
 
 import SwiftUI
 
+
+
 struct CustomButton {
-    
     public enum CustomButtonStyle {
         case plain
+        case check
+        case request
+        case login
+        case logout
+        case signUp
+        case verifyEmail
     }
    
-    
+    let customButton: CustomButtonView
     
     struct CustomButtonView: View {
         public let style: CustomButtonStyle
         public let action: () -> Void
+        private var buttonText: String {
+            switch style {
+            case .plain:
+                return "확인"
+            case .check:
+                return "출석하기"
+            case .request:
+                return "제출하기"
+            case .login:
+                return "로그인"
+            case .logout:
+                return "로그아웃"
+            case .signUp:
+                return "회원가입"
+            case .verifyEmail:
+                return "확인"
+            }
+            
+        }
+        private var screenSizeRatio: (Double, Double) {
+            if style == .verifyEmail {
+                return (0.15, 0.04)
+            } else {
+                return (0.8, 0.07)
+            }
+        }
+        
+        private var buttonTextSize: Font {
+            if style == .verifyEmail {
+                return .subheadline.bold()
+            } else {
+                return .title3.bold()
+            }
+        }
+        
         var body: some View {
-            VStack {
-                
+            Button(action: action) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(.customGreen)
+                        .frame(width: UIScreen.screenWidth * screenSizeRatio.0, height: UIScreen.screenHeight * screenSizeRatio.1)
+                    
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .stroke(Color.customLightGreen, lineWidth: 3)
+//                        .frame(width: UIScreen.screenWidth * 0.8, height: UIScreen.screenHeight * 0.07)
+                    
+                    Text("\(buttonText)")
+                        .font(buttonTextSize)
+                        .foregroundColor(.white)
+                    
+                }
             }
         }
     }
-    init() {
-        
+    
+    init(style: CustomButtonStyle, action: @escaping () -> Void) {
+        customButton = .init(style: style, action: action)
     }
 }
 
