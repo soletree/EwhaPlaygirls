321be74342ccaf835f57d4f1793c67fbf3d70a7c
Hyemin Heo <heohm128@gmail.com>
2023-03-18 04:29:45 +0900

    [Feat] 오늘 예정된 일정 패치 메서드 구현

--- CheckPlay/CheckPlay/ViewModel/ScheduleStore.swift
+++ CheckPlay/CheckPlay/ViewModel/ScheduleStore.swift
@@ -12,6 +12,47 @@ import Firebase
 class ScheduleStore: ObservableObject {
     let database = Firestore.firestore()
     @Published var schedules: [Schedule] = []
+    @Published var scheduleOfToday: Schedule?
+    
+    //MARK: - Method(fetchScheduleOnToday)
+    /// 오늘로 등록되어 있는 스케줄을 fetch 합니다. 오늘로 등록된 스케줄이 여러개인 경우 fetch 시점과 가장 근접한 스케줄 하나만 fetch 해옵니다.
+    func fetchScheduleOnToday() async -> Bool {
+        let calendar = Calendar.current
+        let today = Date().returnDateOfToday()
+        let startDate = calendar.date(from: DateComponents(year: today.0, month: today.1, day: today.2)) ?? Date()
+        let endDate =  calendar.date(from: DateComponents(year: today.0, month: today.1, day: today.2 + 1)) ?? Date()
+        
+        do {
+            let snapshot = try await database.collection("Schedule")
+                .whereField(ScheduleConstant.date, isGreaterThanOrEqualTo: Timestamp(date: startDate))
+                .whereField(ScheduleConstant.date, isLessThan: Timestamp(date: endDate))
+                .getDocuments()
+            
+            if snapshot.documents.isEmpty {
+                DispatchQueue.main.async {
+                    self.scheduleOfToday = nil
+                }
+                return true
+            }
+            let document = snapshot.documents.first!
+            let date = document[ScheduleConstant.date] as? Timestamp
+            let todaySchedule = Schedule(id: document[ScheduleConstant.id] as? String ?? DefaultValue.defaultString,
+                                         date: date?.dateValue() ?? Date(),
+                                         latitude: document[ScheduleConstant.latitude] as? Double ?? 0,
+                                         longitude: document[ScheduleConstant.longitude] as? Double ?? 0,
+                                         address: document[ScheduleConstant.address] as? String ?? DefaultValue.defaultString,
+                                         detailedAddress: document[ScheduleConstant.detailedAddress] as? String ?? DefaultValue.defaultString)
+            
+            DispatchQueue.main.async {
+                self.scheduleOfToday = todaySchedule
+            }
+            
+            return true
+        } catch {
+            print("\(error.localizedDescription)")
+            return false
+        }
+    } // - fetchScheduleOnToday
     
     //MARK: - Method(fetchSchedulesUntilThreeDaysLater)
     /// 현재로부터 3일 뒤의 스케줄만 fetch 해오는 메서드입니다.
