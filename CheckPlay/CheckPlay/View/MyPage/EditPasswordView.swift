ea646f132b510ec28cc8ba39b8f8b9596c0dcc45
Hyemin Heo <heohm128@gmail.com>
2023-03-18 04:30:53 +0900

    [Style] 비밀번호 변경 뷰 구현

---
+++ CheckPlay/CheckPlay/View/MyPage/EditPasswordView.swift
@@ -0,0 +1,44 @@
+//
+//  EditProfileView.swift
+//  CheckPlay
+//
+//  Created by sole on 2023/03/16.
+//
+
+import SwiftUI
+
+//MARK: - View(EditPasswordView)
+/// 비밀번호 변경 뷰입니다.
+struct EditPasswordView: View {
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var userStore: UserStore
+    
+    @State var password: String = ""
+    @State var confirmPassword: String = ""
+    var body: some View {
+        VStack {
+            
+            CustomTextField(style: .secure, title: "비밀번호를 입력하세요", text: $password).customTextField
+            CustomTextField(style: .secure, title: "한 번 더 입력하세요.", text: $confirmPassword).customTextField
+            
+            CustomButton(style: .plain) {
+                if password != confirmPassword {
+                    return
+                }
+                // update password
+                Task {
+                    let result = await userStore.updatePassword(updatedPassword: password)
+                    
+                    if result { dismiss() }
+                }
+            }.customButton
+                .navigationTitle("비밀번호 변경")
+                .navigationBarTitleDisplayMode(.large)
+        } // - VStack
+    }
+}
+
+
+
+
+
