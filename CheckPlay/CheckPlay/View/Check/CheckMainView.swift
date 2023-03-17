b8bec8fb7b11d73a08400726f3cd720d305e44dc
Hyemin Heo <heohm128@gmail.com>
2023-03-18 04:31:54 +0900

    [Style] 오늘 예정된 일정 보여주기 구현

--- CheckPlay/CheckPlay/View/Check/CheckMainView.swift
+++ CheckPlay/CheckPlay/View/Check/CheckMainView.swift
@@ -10,8 +10,21 @@ import MapKit
 
 struct CheckMainView: View {
     @EnvironmentObject var userStore: UserStore
+    @EnvironmentObject var scheduleStore: ScheduleStore
+    @EnvironmentObject var attendanceStore: AttendanceStore
+    
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: 128), latitudinalMeters: 500, longitudinalMeters: 500)
-    @State var isActiveMyPage: Bool = false
+    @State var isActiveNFCSheet: Bool = false
+    var scheduleOfToday: Schedule? {
+        scheduleStore.scheduleOfToday
+    }
+    var circleCenter: CLLocationCoordinate2D {
+        if scheduleOfToday == nil {
+            return .init(latitude: 0, longitude: 0)
+        } else {
+            return scheduleOfToday!.location
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -20,21 +33,41 @@ struct CheckMainView: View {
                 .font(.title.bold())
                 .padding(10)
             
+            if scheduleStore.scheduleOfToday != nil {
+                Text(scheduleOfToday!.date.toStringOnlyHourAndMinute())
+                Text("\(scheduleOfToday!.address)")
+                     +
+                Text("\(scheduleOfToday!.detailedAddress)")
+                    
+            } else {
+                Text("오늘은 일정이 없습니다.")
+                    .padding(10)
+            }
+                
+            
             // 지도
-            CheckMapView()
+            CheckMapView(circleCenter: circleCenter)
     
 //            // nfc 버튼
-//                .toolbar {
-//                    ToolbarItem {
-//                        Button(action: {
-//                            isActiveMyPage = true
-//                        }) {
-//                            Image(systemName: "person.circle.fill")
-//                                .font(.title3)
-//                        }
-//                    } // - ToolbarItem
-//                } // - toolbar
+                .toolbar {
+                    ToolbarItem {
+                        Button(action: {
+                            isActiveNFCSheet = true
+                        }) {
+                            Image(systemName: "sensor.tag.radiowaves.forward.fill")
+                        }
+                    }
+                }
+//               
         } // - VStack
+        .sheet(isPresented: $isActiveNFCSheet) {
+            if #available (iOS 16.0, *) {
+                Text("NFC")
+                    .presentationDetents([.medium])
+            } else {
+                Text("NFC")
+            }
+        }
 
     }
 }
