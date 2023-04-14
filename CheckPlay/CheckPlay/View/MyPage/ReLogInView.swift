3d7402411b026c871df1158305484eda5e17b70e
Hyemin Heo <heohm128@gmail.com>
2023-04-15 06:28:22 +0900

    [Style] 패딩 추가

--- CheckPlay/CheckPlay/View/MyPage/ReLogInView.swift
+++ CheckPlay/CheckPlay/View/MyPage/ReLogInView.swift
@@ -18,6 +18,7 @@ struct ReLogInView: View {
     var body: some View {
         VStack {
             CustomTextField(style: .secure, title: "비밀번호를 입력하세요", text: $password).customTextField
+                .padding(.bottom, 10)
             
             CustomButton(style: .plain) {
                 Task {
