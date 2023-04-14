b7a7f512eff63456d4aaa12542b6194ea8f46ab6
Hyemin Heo <heohm128@gmail.com>
2023-04-15 04:11:42 +0900

    [Feat] 위치 기반 출석체크 로직 구현

--- CheckPlay/CheckPlay/View/Check/Map/CheckMapView.swift
+++ CheckPlay/CheckPlay/View/Check/Map/CheckMapView.swift
@@ -8,18 +8,132 @@
 import SwiftUI
 import NMapsMap
 import CoreLocation
+import AlertToast
 
 struct CheckMapView: View {
+    @EnvironmentObject var scheduleStore: ScheduleStore
+    @EnvironmentObject var attendanceStore: AttendanceStore
     var circleCenter: CLLocationCoordinate2D = .init(latitude: 37.5666102, longitude: 126.9783881)
     var circleRadius: Double = 20
+    var locationManager = CLLocationManager()
+    @State var isPresentedAttendanceAlert: Bool = false
+    @State var attendanceAlert = AlertToast(displayMode: .alert, type: .complete(.green))
+    
+    
     var body: some View {
-        NaverMapView(circleCenter: circleCenter, circleRadius: circleRadius)
+        VStack {
+            switch locationManager.authorizationStatus {
+            case .authorizedAlways, .authorizedWhenInUse:
+                checkMapView
+            case .denied, .notDetermined, .restricted:
+                checkLocationAuthorization
+            }
+        } // - VStack
+        .onAppear {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        .toast(isPresenting: $isPresentedAttendanceAlert) {
+            attendanceAlert
+        }
     }
+    
+    private var checkLocationAuthorization: some View {
+        Button(action: {
+            locationManager.requestWhenInUseAuthorization()
+        }) {
+            Image(systemName: "location.square.fill")
+                .resizable()
+                .frame(width: UIScreen.screenWidth * 0.5, height: UIScreen.screenWidth * 0.5)
+        }
+    }
+    
+    private var checkMapView: some View {
+        VStack {
+            NaverMapView(circleCenter: circleCenter, circleRadius: circleRadius)
+            
+            CustomButton(style: .check) {
+                switch isVaildCheck() {
+                case .success(let result):
+                    Task {
+                        guard let attendanceOfToday = attendanceStore.attendanceOfToday
+                        else { return }
+                        
+                        // remote 변경 사항
+                        guard await attendanceStore.updateAttendance(attendanceID: attendanceOfToday.id, attendanceStatus: result.0, lateTime: result.1)
+                        else { return }
+                        
+                        
+                        // local 변경 사항
+                        attendanceStore.attendanceOfToday?.attendanceStatus = result.0
+                        attendanceStore.attendanceOfToday?.lateTime = result.1
+                        
+                        // alert 처리
+                        attendanceAlert.type = .complete(.green)
+                        attendanceAlert.title = "출석체크가 완료되었습니다."
+                        isPresentedAttendanceAlert = true
+                    }
+                case .failure(let error):
+                    // error typing
+                    switch error {
+                    case .location:
+                        attendanceAlert.title = "지정된 위치가 아닙니다.\n \(locationManager.location?.distance(to: circleCenter) ?? 0)m 이동해주세요!"
+                    case .time:
+                        attendanceAlert.title = "출석체크 시간이 아닙니다!"
+                    case .complete:
+                        attendanceAlert.title = "이미 출석체크가 완료되었습니다!"
+                    case .unknown:
+                        attendanceAlert.title = "알 수 없는 오류입니다! 개발자에게 문의하세요."
+                    case .noSchedule:
+                        attendanceAlert.title = "오늘은 일정이 없습니다!"
+                    }
+                    
+                    attendanceAlert.type = .regular
+                    isPresentedAttendanceAlert = true
+                }
+            }.customButton
+                .padding(.vertical, 20)
+        }
+    } // - checkMapView
+    
+    private func isVaildCheck() -> Result<(AttendanceStatus, Int),AttendanceError> {
+        guard let attendanceOfToday = attendanceStore.attendanceOfToday
+        else { return .failure(.noSchedule) }
+        
+        if attendanceOfToday.attendanceStatus != .absent {
+            return .failure(.complete)
+        }
+        
+        guard let currentLocation = locationManager.location
+        else { return .failure(.unknown) }
+        // time 조건
+        guard let comparedDate = scheduleStore.scheduleOfToday?.date else { return .failure(.noSchedule) }
+        
+        guard let timeComparedResult = Date().compareToSpecificDate(compared: comparedDate)
+        else { return .failure(.time) }
+        
+        
+        // 모임 장소로부터 거리가 20m 초과일시 출석체크하지 않습니다.
+        if currentLocation.distance(to: circleCenter) > 20 {
+            return .failure(.location)
+        }
+        
+        
+        return .success(timeComparedResult)
+    }
+        
+}
+
+enum AttendanceError: Error {
+    case time
+    case location
+    case complete
+    case unknown
+    case noSchedule
 }
 
 
 struct NaverMapView: UIViewRepresentable {
-    let mapController = NaverMapViewController()
+    let mapController: NaverMapViewController = .init()
     var circleCenter: CLLocationCoordinate2D
     var circleRadius: Double
     
@@ -33,6 +147,7 @@ struct NaverMapView: UIViewRepresentable {
         view.mapView.minZoomLevel = 5
         view.mapView.maxZoomLevel = 20
         
+        // 현재 위치 focus
         view.mapView.latitude = mapController.locationManager.location?.coordinate.latitude ?? 0
         view.mapView.longitude = mapController.locationManager.location?.coordinate.longitude ?? 0
         // 실내 지도 모드 활성화
@@ -41,6 +156,9 @@ struct NaverMapView: UIViewRepresentable {
         // 현재 위치 포커스 버튼 활성화
         view.showLocationButton = true
         
+        // 모임 장소에 핀을 꽂습니다.
+        let pin = NMFMarker(position: NMGLatLng(lat: circleCenter.latitude, lng: circleCenter.longitude))
+        pin.mapView = view.mapView
         
         let circle = NMFCircleOverlay()
         circle.center = NMGLatLng(lat: circleCenter.latitude, lng: circleCenter.longitude)
@@ -54,6 +172,7 @@ struct NaverMapView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
+       
     }
     
 
@@ -62,18 +181,19 @@ struct NaverMapView: UIViewRepresentable {
 class NaverMapViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager: CLLocationManager = .init()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let mapView = NMFMapView(frame: view.frame)
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
+        checkLocationAuthorization()
+        
         
         if CLLocationManager.locationServicesEnabled() {
             self.locationManager.startUpdatingLocation()
-            
+           
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: self.locationManager.location?.coordinate.latitude ?? 0, lng: self.locationManager.location?.coordinate.longitude ?? 0))
             cameraUpdate.animation = .easeIn
             mapView.moveCamera(cameraUpdate)
@@ -85,14 +205,18 @@ class NaverMapViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        
-        // 모임 위치와의 거리 계산
-        
-        print(location)
+    func checkLocationAuthorization() {
+        switch self.locationManager.authorizationStatus {
+        case .restricted, .notDetermined, .denied, .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization()
+        default:
+            return
+        }
     }
+
 }
+
+
 
 
 class Coordinator: NSObject, NMFMapViewCameraDelegate, NMFMapViewTouchDelegate, NMFMapViewOptionDelegate {
@@ -100,8 +224,8 @@ class Coordinator: NSObject, NMFMapViewCameraDelegate, NMFMapViewTouchDelegate, 
 }
 
 extension CLLocation {
-    func distance(from: CLLocationCoordinate2D, to: CLLocationCoordinate2D) -> Int {
-        distance(from: CLLocation(latitude: from.latitude, longitude: from.longitude))
-        return 1
+    
+    func distance(to: CLLocationCoordinate2D) -> Int {
+        return Int(self.distance(from: CLLocation(latitude: to.latitude, longitude: to.longitude)))
     }
 }
