fa9147c515763743855b929b1ca8bbc814030832
Hyemin Heo <heohm128@gmail.com>
2023-04-15 04:11:57 +0900

    [Feat] NFC 기능 구현

--- CheckPlay/CheckPlay/View/Check/NFCSheetView.swift
+++ CheckPlay/CheckPlay/View/Check/NFCSheetView.swift
@@ -7,16 +7,68 @@
 
 import SwiftUI
 import CoreNFC
+import SwiftNFC
 
 struct NFCSheetView: View {
-    
+    @ObservedObject var nfcReader = NFCReader()
+    @ObservedObject var nfcWriter = NFCWriter()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .onAppear {
-                print(NFCReaderSession.readingAvailable)
+                nfcReader.startAlert = "핸드폰을 NFC 태그에 가까이 해주세요."
+                
+                nfcReader.read()
+                print(nfcReader.raw)
+                print(nfcReader.msg)
             }
     }
         
 }
 
 
+@available(iOS 13.0, *)
+public class NFCReader: NSObject, ObservableObject, NFCNDEFReaderSessionDelegate {
+    
+    public var startAlert = "NFC 태그를 핸드폰 가까이에 두세요."
+    public var endAlert = ""
+    @Published public var msg = "Scan to read or Edit here to write..."
+    public var raw = "Raw Data available after scan."
+
+    public var session: NFCNDEFReaderSession?
+    
+    public func read() {
+        guard NFCNDEFReaderSession.readingAvailable else {
+            print("Error")
+            return
+        }
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        session?.alertMessage = self.startAlert
+        session?.begin()
+    }
+    
+    public func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        DispatchQueue.main.async {
+            self.msg = messages.map {
+                $0.records.map {
+                    String(decoding: $0.payload, as: UTF8.self)
+                }.joined(separator: "\n")
+            }.joined(separator: " ")
+            
+            self.raw = messages.map {
+                $0.records.map {
+                    "\($0.typeNameFormat) \(String(decoding:$0.type, as: UTF8.self)) \(String(decoding:$0.identifier, as: UTF8.self)) \(String(decoding: $0.payload, as: UTF8.self))"
+                }.joined(separator: "\n")
+            }.joined(separator: " ")
+            
+            session.alertMessage = self.endAlert != "" ? self.endAlert : "NFC 태그 읽기 성공"
+        }
+    }
+    
+    public func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
+    }
+    
+    public func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("Session did invalidate with error: \(error)")
+        self.session = nil
+    }
+}
