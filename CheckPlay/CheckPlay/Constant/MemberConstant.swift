e4cf7cf9d383dbbf05bee92e4137cbbdb541e0cf
Hyemin Heo <heohm128@gmail.com>
2023-08-29 02:53:33 +0900

    [Add] Firebase에서 사용하는 MemberConstant 추가

---
+++ CheckPlay/CheckPlay/Constant/MemberConstant.swift
@@ -0,0 +1,14 @@
+//
+//  MemberConstant.swift
+//  EwhaPlaygirls-admin
+//
+//  Created by sole on 2023/03/04.
+//
+
+import Foundation
+
+struct MemberConstant {
+    static let studentCode = "student_code"
+    static let name = "name"
+    static let memberStatus = "member_status"
+}
