7453a94430539d8cecaca06691ffe1bcdd1dbf71
Hyemin Heo <heohm128@gmail.com>
2024-01-30 01:55:56 +0900

    [Style] 새로운 버튼 스타일 적용

--- CheckPlay/CheckPlay/View/Customer/Map/CheckMapView.swift
+++ CheckPlay/CheckPlay/View/Customer/Map/CheckMapView.swift
@@ -13,9 +13,12 @@ import AlertToast
 struct CheckMapView: View {
     @EnvironmentObject var scheduleStore: ScheduleStore
     @EnvironmentObject var attendanceStore: AttendanceStore
-    var circleCenter: CLLocationCoordinate2D = .init(latitude: 37, longitude: 126)
-    var circleRadius: Double = 30
+    
+    var circleCenter: CLLocationCoordinate2D = .init(latitude: 37,
+                                                     longitude: 126)
+    var circleRadius: Double = 50
     var locationManager = CLLocationManager()
+    
     @State var isPresentedAttendanceAlert: Bool = false
     @State var attendanceAlert = AlertToast(displayMode: .alert,
                                             type: .complete(.green))
@@ -27,7 +30,7 @@ struct CheckMapView: View {
             case .authorizedAlways, .authorizedWhenInUse:
                 checkMapView
             case .denied, .notDetermined, .restricted:
-                checkLocationAuthorization
+                locationAuthorizationView
             }
         } // - VStack
         .onAppear {
@@ -38,65 +41,60 @@ struct CheckMapView: View {
         }
     }
     
-    private var checkLocationAuthorization: some View {
-        Button(action: {
+    //MARK: - checkLocationAuthorization
+    private var locationAuthorizationView: some View {
+        Button {
             locationManager.requestWhenInUseAuthorization()
-        }) {
+        } label: {
             Image(systemName: "location.square.fill")
                 .resizable()
-                .frame(width: UIScreen.screenWidth * 0.5, height: UIScreen.screenWidth * 0.5)
+                .frame(width: 200,
+                       height: 200)
+                .foregroundStyle(Color.brandColor)
         }
     }
     
     private var checkMapView: some View {
         VStack {
-            NaverMapView(circleCenter: circleCenter, circleRadius: circleRadius)
+            NaverMapView(circleCenter: circleCenter,
+                         circleRadius: circleRadius)
             
-//            CustomButton(style: .check) {
-//                switch isVaildCheck() {
-//                case .success(let result):
-//                    Task {
-//                        guard let attendanceOfToday = attendanceStore.attendanceOfToday
-//                        else { return }
-//                        
-//                        // remote 변경 사항
-//                        guard await attendanceStore.updateAttendance(attendanceID: attendanceOfToday.id, attendanceStatus: result.0, lateTime: result.1)
-//                        else { return }
-//                        
-//                        
-//                        // local 변경 사항
-//                        attendanceStore.attendanceOfToday?.attendanceStatus = result.0
-//                        attendanceStore.attendanceOfToday?.lateTime = result.1
-//                        
-//                        // alert 처리
-//                        attendanceAlert.type = .complete(.green)
-//                        attendanceAlert.title = "출석체크가 완료되었습니다."
-//                        isPresentedAttendanceAlert = true
-//                    }
-//                case .failure(let error):
-//                    // error typing
-//                    switch error {
-//                    case .location:
-//                        attendanceAlert.title = "지정된 위치가 아닙니다.\n \(locationManager.location?.distance(to: circleCenter) ?? 0)m 이동해주세요!"
-//                    case .time:
-//                        attendanceAlert.title = "출석체크 시간이 아닙니다!"
-//                    case .complete:
-//                        attendanceAlert.title = "이미 출석체크가 완료되었습니다!"
-//                    case .unknown:
-//                        attendanceAlert.title = "알 수 없는 오류입니다! 개발자에게 문의하세요."
-//                    case .noSchedule:
-//                        attendanceAlert.title = "오늘은 일정이 없습니다!"
-//                    }
-//                    
-//                    attendanceAlert.type = .regular
-//                    isPresentedAttendanceAlert = true
-//                }
-//            }.customButton
-//                .padding(.vertical, 20)
+            EPButton {
+                switch isVaildCheck() {
+                case .success(let result):
+                    Task {
+                        guard let attendanceOfToday = attendanceStore.attendanceOfToday
+                        else { return }
+                        
+                        // remote 변경 사항
+                        guard await attendanceStore.updateAttendance(attendanceID: attendanceOfToday.id, attendanceStatus: result.status, lateTime: result.lateTime)
+                        else { return }
+                        
+                        
+                        // local 변경 사항
+                        attendanceStore.attendanceOfToday?.attendanceStatus = result.status
+                        attendanceStore.attendanceOfToday?.lateTime = result.lateTime
+                        
+                        // alert 처리
+                        attendanceAlert.type = .complete(.green)
+                        attendanceAlert.title = "출석체크가 완료되었습니다."
+                        isPresentedAttendanceAlert = true
+                    }
+                case .failure(let error):
+                    attendanceAlert.title = error.toString()
+                    attendanceAlert.type = .regular
+                    isPresentedAttendanceAlert = true
+                }
+            } label: { 
+                Text("출석하기")
+                    .frame(maxWidth: .infinity)
+            }
+            .padding(.horizontal, 20)
         }
     } // - checkMapView
     
-    private func isVaildCheck() -> Result<(AttendanceStatus, Int),AttendanceError> {
+    //MARK: - isVaildCheck
+    private func isVaildCheck() -> Result<(status: AttendanceStatus, lateTime: Int), AttendanceError> {
         guard let attendanceOfToday = attendanceStore.attendanceOfToday
         else { return .failure(.noSchedule) }
         
@@ -107,29 +105,19 @@ struct CheckMapView: View {
         guard let currentLocation = locationManager.location
         else { return .failure(.unknown) }
         // time 조건
-        guard let comparedDate = scheduleStore.scheduleOfToday?.date else { return .failure(.noSchedule) }
+        guard let comparedDate = scheduleStore.scheduleOfToday?.date 
+        else { return .failure(.noSchedule) }
         
         guard let timeComparedResult = Date().compareToSpecificDate(compared: comparedDate)
         else { return .failure(.time) }
         
-        
-        // 모임 장소로부터 거리가 20m 초과일시 출석체크하지 않습니다.
-        if currentLocation.distance(to: circleCenter) > 20 {
+        // 모임 장소로부터 거리가 50m 초과일시 출석체크하지 않습니다.
+        if currentLocation.distance(to: circleCenter) > Int(circleRadius) {
             return .failure(.location)
         }
         
-        
         return .success(timeComparedResult)
     }
-        
-}
-
-enum AttendanceError: Error {
-    case time
-    case location
-    case complete
-    case unknown
-    case noSchedule
 }
 
 
@@ -158,11 +146,13 @@ struct NaverMapView: UIViewRepresentable {
         view.showLocationButton = true
         
         // 모임 장소에 핀을 꽂습니다.
-        let pin = NMFMarker(position: NMGLatLng(lat: circleCenter.latitude, lng: circleCenter.longitude))
+        let pin = NMFMarker(position: NMGLatLng(lat: circleCenter.latitude,
+                                                lng: circleCenter.longitude))
         pin.mapView = view.mapView
         
         let circle = NMFCircleOverlay()
-        circle.center = NMGLatLng(lat: circleCenter.latitude, lng: circleCenter.longitude)
+        circle.center = NMGLatLng(lat: circleCenter.latitude,
+                                  lng: circleCenter.longitude)
         // 미터 단위로 원이 그려집니다.
         circle.radius = circleRadius
         circle.outlineColor = UIColor(Color.mapCircleOutlineRed)
@@ -175,13 +165,11 @@ struct NaverMapView: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
        
     }
-    
-
 }
 
-class NaverMapViewController: UIViewController, CLLocationManagerDelegate {
+class NaverMapViewController: UIViewController,
+                              CLLocationManagerDelegate {
     var locationManager: CLLocationManager = .init()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -191,21 +179,18 @@ class NaverMapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         checkLocationAuthorization()
         
-        
         if CLLocationManager.locationServicesEnabled() {
             self.locationManager.startUpdatingLocation()
            
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: self.locationManager.location?.coordinate.latitude ?? 0, lng: self.locationManager.location?.coordinate.longitude ?? 0))
             cameraUpdate.animation = .easeIn
             mapView.moveCamera(cameraUpdate)
-            
         } else {
             print("위치 서비스 off")
         }
-        
-        
     }
     
+    //MARK: - checkLocationAuthorization
     func checkLocationAuthorization() {
         switch self.locationManager.authorizationStatus {
         case .restricted, .notDetermined, .denied, .notDetermined:
@@ -214,19 +199,10 @@ class NaverMapViewController: UIViewController, CLLocationManagerDelegate {
             return
         }
     }
-
 }
 
-
-
-
-class Coordinator: NSObject, NMFMapViewCameraDelegate, NMFMapViewTouchDelegate, NMFMapViewOptionDelegate {
-    
-}
-
-extension CLLocation {
-    
-    func distance(to: CLLocationCoordinate2D) -> Int {
-        return Int(self.distance(from: CLLocation(latitude: to.latitude, longitude: to.longitude)))
-    }
+#Preview {
+    CheckMapView()
+        .environmentObject(ScheduleStore())
+        .environmentObject(AttendanceStore())
 }
