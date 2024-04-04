41583d34372acba785b7ce2ece1b63380eda8360
Hyemin Heo <heohm128@gmail.com>
2024-04-05 01:46:59 +0900

    [Chore] 폴더 이동

--- CheckPlay/CheckPlay/ViewModel/AttendanceStore.swift
+++ CheckPlay/CheckPlay/ViewModel/AttendanceStore.swift
@@ -9,6 +9,9 @@ import Foundation
 import FirebaseFirestore
 
 class AttendanceStore: ObservableObject {
+    private var collectionName: String {
+        return "Attendance\(Date.yearOfToday)"
+    }
     let database = Firestore.firestore()
     @Published var attendances: [Attendance] = []
     @Published var attendanceOfToday: Attendance?
@@ -16,7 +19,7 @@ class AttendanceStore: ObservableObject {
     
     func fetchAttendance(attendanceID: String) async -> Result<Attendance, StoreError> {
         do {
-            let document = try await database.collection("Attendance")
+            let document = try await database.collection(collectionName)
                 .document(attendanceID)
                 .getDocument()
             let timestamp = document[AttendanceConstant.date] as? Timestamp
@@ -38,12 +41,14 @@ class AttendanceStore: ObservableObject {
     }
     
     //MARK: - Method(fetchAttendance)
+    @discardableResult
     @MainActor
     func fetchAttendances(studentCode: String) async -> Bool {
         var fetchAttendances: [Attendance] = []
         do {
-            let snapshot = try await database.collection("Attendance")
-                .whereField(AttendanceConstant.studentCode, isEqualTo: studentCode)
+            let snapshot = try await database.collection(collectionName)
+                .whereField(AttendanceConstant.studentCode,
+                            isEqualTo: studentCode)
                 .getDocuments()
             
             // 문서가 없음. fetch 자체는 성공이라 true 반환
@@ -76,68 +81,70 @@ class AttendanceStore: ObservableObject {
         }
     } // - fetchAttendance
     
-    //MARK: - Method(fetchScheduleInPeriod)
-    func fetchScheduleInPeriod(year: Int,
-                               period: Period) async -> Bool {
-        let calendar = Calendar.current
-        let nowTimeStamp = Timestamp(date: .init())
-        var startDate: Date = .init()
-        var endDate: Date = .init()
-        
-        switch period {
-        case .firstHalf:
-            startDate = calendar.date(from: DateComponents(year: year,
-                                                           month: 3,
-                                                           day: 1)) ?? Date()
-            endDate = calendar.date(from: DateComponents(year: year,
-                                                         month: 9 ,
-                                                         day: 1)) ?? Date()
-        case.secondHalf:
-            startDate = calendar.date(from: DateComponents(year: year,
-                                                           month: 9,
-                                                           day: 1)) ?? Date()
-            endDate = calendar.date(from: DateComponents(year: year + 1,
-                                                         month: 3,
-                                                         day: 1)) ?? Date()
-        }
-        
-        do {
-            let snapshot = try await database.collection("Attendance")
-                .whereField(ScheduleConstant.date, isGreaterThanOrEqualTo: Timestamp(date: startDate))
-                .whereField(ScheduleConstant.date, isLessThanOrEqualTo: nowTimeStamp)
-                .whereField(ScheduleConstant.date, isLessThan: Timestamp(date: endDate))
-                .getDocuments()
-            
-            if snapshot.documents.isEmpty { return true }
-            
-            for document in snapshot.documents {
-                let timestamp = document[AttendanceConstant.date] as? Timestamp
-                let attendance = Attendance(id: document[AttendanceConstant.id] as? String ?? "",
-                                            studentCode: document[AttendanceConstant.studentCode] as? String ?? "",
-                                            scheduleID: document[AttendanceConstant.scheduleID] as? String ?? "",
-                                            attendanceStatus: AttendanceStatus(rawValue: document[AttendanceConstant.attendanceStatus] as? String ?? "") ?? .absent,
-                                            lateTime: document[AttendanceConstant.lateTime] as? Int ?? 0,
-                                            isPaying: document[AttendanceConstant.isPaying] as? Bool ?? false,
-                                            date: timestamp?.dateValue() ?? Date())
-            }
-            return true
-            
-        } catch {
-            print("\(error.localizedDescription)")
-            return false
-        }
-    } // - fetchScheduleInPeriod
+//    //MARK: - Method(fetchScheduleInPeriod)
+//    func fetchScheduleInPeriod(year: Int,
+//                               period: Period) async -> Bool {
+//        let calendar = Calendar.current
+//        let nowTimeStamp = Timestamp(date: .init())
+//        var startDate: Date = .init()
+//        var endDate: Date = .init()
+//        
+//        switch period {
+//        case .firstHalf:
+//            startDate = calendar.date(from: DateComponents(year: year,
+//                                                           month: 3,
+//                                                           day: 1)) ?? Date()
+//            endDate = calendar.date(from: DateComponents(year: year,
+//                                                         month: 9 ,
+//                                                         day: 1)) ?? Date()
+//        case.secondHalf:
+//            startDate = calendar.date(from: DateComponents(year: year,
+//                                                           month: 9,
+//                                                           day: 1)) ?? Date()
+//            endDate = calendar.date(from: DateComponents(year: year + 1,
+//                                                         month: 3,
+//                                                         day: 1)) ?? Date()
+//        }
+//        
+//        do {
+//            let snapshot = try await database.collection(collectionName)
+//                .whereField(ScheduleConstant.date, isGreaterThanOrEqualTo: Timestamp(date: startDate))
+//                .whereField(ScheduleConstant.date,
+//                            isLessThanOrEqualTo: nowTimeStamp)
+//                .whereField(ScheduleConstant.date,
+//                            isLessThan: Timestamp(date: endDate))
+//                .getDocuments()
+//            
+//            if snapshot.documents.isEmpty { return true }
+//            
+//            for document in snapshot.documents {
+//                let timestamp = document[AttendanceConstant.date] as? Timestamp
+//                let attendance = Attendance(id: document[AttendanceConstant.id] as? String ?? "",
+//                                            studentCode: document[AttendanceConstant.studentCode] as? String ?? "",
+//                                            scheduleID: document[AttendanceConstant.scheduleID] as? String ?? "",
+//                                            attendanceStatus: AttendanceStatus(rawValue: document[AttendanceConstant.attendanceStatus] as? String ?? "") ?? .absent,
+//                                            lateTime: document[AttendanceConstant.lateTime] as? Int ?? 0,
+//                                            isPaying: document[AttendanceConstant.isPaying] as? Bool ?? false,
+//                                            date: timestamp?.dateValue() ?? Date())
+//            }
+//            return true
+//            
+//        } catch {
+//            print("\(error.localizedDescription)")
+//            return false
+//        }
+//    } // - fetchScheduleInPeriod
     
     //MARK: - Method(updateAttendance)
     func updateAttendance(attendanceID: String,
                           attendanceStatus: AttendanceStatus,
                           lateTime: Int = 0) async -> Bool {
         do {
-            try await database.collection("Attendance")
+            try await database.collection(collectionName)
                 .document(attendanceID)
                 .updateData([AttendanceConstant.attendanceStatus : attendanceStatus.rawValue])
                 
-            try await database.collection("Attendance")
+            try await database.collection(collectionName)
                 .document(attendanceID)
                 .updateData([AttendanceConstant.lateTime : lateTime])
             
@@ -151,8 +158,9 @@ class AttendanceStore: ObservableObject {
     func findAttendanceWithScheduleID(scheduleID: String,
                                       studentCode: String) async -> Result<String, StoreError> {
         do {
-            let snapshot = try await database.collection("Attendance")
-                .whereField(AttendanceConstant.scheduleID, isEqualTo: scheduleID)
+            let snapshot = try await database.collection(collectionName)
+                .whereField(AttendanceConstant.scheduleID,
+                            isEqualTo: scheduleID)
                 .getDocuments()
             
             if snapshot.documents.isEmpty { return .failure(.fetchError) }
@@ -173,7 +181,7 @@ class AttendanceStore: ObservableObject {
     //MARK: - updateAttendance(Method)
     func updateAttendance(attendance: Attendance) async -> Bool {
         do {
-            try await database.collection("Attedance")
+            try await database.collection(collectionName)
                 .document(attendance.id)
                 .setData([
                     AttendanceConstant.id : attendance.id,
