73ee9fd520a60b18e5b49dcae0e632406d758556
Hyemin Heo <heohm128@gmail.com>
2024-04-05 01:50:35 +0900

    [Style] 지도 뷰 비콘 뷰로 대체

--- CheckPlay/CheckPlay/View/Customer/Check/CheckMainView.swift
+++ CheckPlay/CheckPlay/View/Customer/Check/CheckMainView.swift
@@ -15,23 +15,13 @@ struct CheckMainView: View {
     @EnvironmentObject var userStore: UserStore
     @EnvironmentObject var scheduleStore: ScheduleStore
     @EnvironmentObject var attendanceStore: AttendanceStore
-    
-    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: 128), latitudinalMeters: 500, longitudinalMeters: 500)
+    @StateObject var alertViewModel: AlertViewModel = .init()
     @ObservedObject var nfcReader = NFCReader()
     
-    @State var isPresentedNFCAlert: Bool = false
-    @State var isProcessingWithNFC: Bool = false
-    @State var alertOfNFC = AlertToast(displayMode: .alert, type: .regular)
+    @State var userRegion: CLRegionState = .unknown
     
     var scheduleOfToday: Schedule? {
         scheduleStore.scheduleOfToday
-    }
-    var circleCenter: CLLocationCoordinate2D {
-        if scheduleOfToday == nil {
-            return .init(latitude: 0, longitude: 0)
-        } else {
-            return scheduleOfToday!.location
-        }
     }
     
     var body: some View {
@@ -42,28 +32,84 @@ struct CheckMainView: View {
                             weight: .semibold)
             
             if scheduleStore.scheduleOfToday != nil {
-                Group {
-                    Text("\(scheduleOfToday!.date.toStringOnlyHourAndMinute())")
-                        .pretendard(size: .s,
-                                    weight: .semibold)
-                    Text("\(scheduleOfToday!.address) \(scheduleOfToday!.detailedAddress)")
-                        .pretendard(size: .s,
-                                    weight: .semibold)
-                } // - VStack
-            } else {
-                Text("오늘은 일정이 없습니다.")
+                Text("\(scheduleOfToday!.date.toStringOnlyHourAndMinute())")
                     .pretendard(size: .s,
-                                weight: .regular)
+                                weight: .semibold)
+               
+            } else {
+                Text("오늘은 일정이 없어요")
+                    .pretendard(size: .s,
+                                weight: .medium)
             }
-                
             
-            // 지도
-            CheckMapView(circleCenter: circleCenter)
-                .overlay {
-                    if isProcessingWithNFC {
-                        ProgressView()
-                    }
+            Text("출석 범위 \(userRegion.toString())이에요")
+                .pretendard(size: .s,
+                            weight: .medium)
+            
+            BeaconCheckView(userRegion: $userRegion)
+            
+            EPButton {
+                // 오늘 일정이 있는 날인지 확인합니다.
+                guard let scheduleOfToday = scheduleOfToday
+                else {
+                    alertViewModel.setErrorTitle(title:  AttendanceError.noSchedule.toString())
+                    alertViewModel.isError = true
+                    return
                 }
+                
+                guard attendanceStore.attendanceOfToday == nil
+                else { alertViewModel.setErrorTitle(title:  AttendanceError.complete.toString())
+                    alertViewModel.isError = true
+                    return
+                }
+                
+                guard userRegion == .inside
+                else {
+                    alertViewModel.setErrorTitle(title:  AttendanceError.location.toString())
+                    alertViewModel.isError = true
+                    return
+                }
+                
+                // time 조건
+                guard let comparedDate = scheduleStore.scheduleOfToday?.date
+                else {
+                    alertViewModel.setErrorTitle(title:  AttendanceError.noSchedule.toString())
+                    alertViewModel.isError = true
+                    return
+                }
+                
+                guard let timeComparedResult = Date().compareToSpecificDate(compared: comparedDate)
+                else {
+                    alertViewModel.setErrorTitle(title:  AttendanceError.time.toString())
+                    alertViewModel.isError = true
+                    return
+                }
+                
+                let attendance = Attendance(id: UUID().uuidString,
+                                            studentCode: userStore.currentUser?.studentCode ?? "",
+                                            scheduleID: scheduleOfToday.id,
+                                            attendanceStatus: timeComparedResult.status,
+                                            lateTime: timeComparedResult.lateTime,
+                                            isPaying: false,
+                                            date: Date())
+                
+                Task {
+                    // remote 변경 사항
+                    guard await attendanceStore.updateAttendance(attendance: attendance)
+                    else { return }
+                }
+                
+                // local 변경 사항
+                attendanceStore.attendanceOfToday = attendance
+                
+                // alert 처리
+                alertViewModel.setCompleteTitle(title: "출석체크가 완료되었어요")
+                alertViewModel.isComplete = true
+            } label: {
+                Text("출석하기")
+                    .frame(maxWidth: .infinity)
+            }
+            .padding(.bottom, 10)
             
             
             // nfc 버튼
@@ -79,14 +125,22 @@ struct CheckMainView: View {
                     }
                 }
         } // - VStack
-        .disabled(isProcessingWithNFC)
-        .toast(isPresenting: $isPresentedNFCAlert) {
-            alertOfNFC
+        .padding(.horizontal, 20)
+        .disabled(alertViewModel.isProcessing)
+        .toast(isPresenting: $alertViewModel.isError) {
+            alertViewModel.errorAlert
+        }
+        .toast(isPresenting: $alertViewModel.isComplete) {
+            alertViewModel.completAlert
         }
         .onChange(of: nfcReader.msg) { message in
             processAttendanceWithNFC(message)
         } // - onChange
 
+    }
+    
+    private func attendanceCheck() {
+        
     }
     
     //MARK: - isValid
@@ -105,7 +159,8 @@ struct CheckMainView: View {
         }
         
         // 시간 체크
-        guard let timeComparedResult = Date().compareToSpecificDate(compared: scheduleOfToday.date) else { return .failure(.time) }
+        guard let timeComparedResult = Date().compareToSpecificDate(compared: scheduleOfToday.date) 
+        else { return .failure(.time) }
         
         return .success(timeComparedResult)
     }
@@ -113,16 +168,16 @@ struct CheckMainView: View {
     //MARK: - processAttendanceWithNFC
     private func processAttendanceWithNFC(_ message: String) {
             // 적절하지 않은 nfc가 태그 됐을 때 아무 동작도 실행하지 않습니다.
-            isProcessingWithNFC = true
+            alertViewModel.isProcessing = true
             guard message == "\(Bundle.main.object(forInfoDictionaryKey: "VALID_NFC_TAG") ?? "")" else {
                 // nfc msg를 초기화하기 위한 조건문입니다.
                 if message == "" {
-                    isProcessingWithNFC = false
+                    alertViewModel.isProcessing = false
                     return
                 }
-                alertOfNFC.title = "잘못된 NFC 태그입니다! 다시 시도하세요."
-                isPresentedNFCAlert = true
-                isProcessingWithNFC = false
+                alertViewModel.setErrorTitle(title: "잘못된 NFC 태그입니다! 다시 시도하세요.")
+                alertViewModel.isProcessing = false
+                alertViewModel.isError = true
                 nfcReader.msg = ""
                 return
             }
@@ -130,10 +185,9 @@ struct CheckMainView: View {
             guard let attendanceOfToday = attendanceStore.attendanceOfToday
             else {
                 // 일정이 없음을 알리는 알럿
-                
-                alertOfNFC.title = "오늘은 일정이 없습니다."
-                isPresentedNFCAlert = true
-                isProcessingWithNFC = false
+                alertViewModel.setErrorTitle(title: "오늘은 일정이 없습니다.")
+                alertViewModel.isError = true
+                alertViewModel.isProcessing = false
                 nfcReader.msg = ""
                 return
             }
@@ -145,35 +199,35 @@ struct CheckMainView: View {
                     let updateAttendanceResult = await attendanceStore.updateAttendance(attendanceID: attendanceOfToday.id, attendanceStatus: result.0, lateTime: result.1)
                     
                     guard updateAttendanceResult else {
-                        alertOfNFC.title = "출석체크가 정상적으로 처리되지 않았습니다. 다시 시도해주세요!"
-                        isPresentedNFCAlert = true
-                        isProcessingWithNFC = false
+                        alertViewModel.setErrorTitle(title: "출석체크가 정상적으로 처리되지 않았습니다. 다시 시도해주세요!")
+                        alertViewModel.isError = true
+                        alertViewModel.isProcessing = false
                         return
                     }
                     // local 출석체크
                     attendanceStore.attendanceOfToday!.attendanceStatus = result.0
                     attendanceStore.attendanceOfToday!.lateTime = result.1
-                    alertOfNFC.type = .complete(.green)
-                    alertOfNFC.title = "출석체크가 완료되었습니다."
+                    alertViewModel.setCompleteTitle(title: "출석체크가 완료되었습니다.")
                 }
                 
             case .failure(let error):
                 switch error {
                 case .time:
-                    alertOfNFC.title = "출석체크 시간이 아닙니다!"
+                    alertViewModel.setErrorTitle(title: "출석체크 시간이 아닙니다!")
                 case .location:
-                    alertOfNFC.title = "지정된 위치가 아닙니다."
+                    alertViewModel.setErrorTitle(title: "지정된 위치가 아닙니다.")
                 case .complete:
-                    alertOfNFC.title = "이미 출석체크가 완료되었습니다!"
+                    alertViewModel.setErrorTitle(title: "이미 출석체크가 완료되었습니다!")
                 case .unknown:
-                    alertOfNFC.title = "알 수 없는 오류입니다! 개발자에게 문의하세요."
+                    alertViewModel.setErrorTitle(title:
+                    "알 수 없는 오류입니다! 개발자에게 문의하세요.")
                 case .noSchedule:
-                    alertOfNFC.title = "오늘은 일정이 없습니다!"
+                    alertViewModel.setErrorTitle(title: "오늘은 일정이 없습니다!")
                 }
             }
             nfcReader.msg = ""
-            isPresentedNFCAlert = true
-            isProcessingWithNFC = false
+            alertViewModel.isError = true
+            alertViewModel.isProcessing = false
     }
 }
 
